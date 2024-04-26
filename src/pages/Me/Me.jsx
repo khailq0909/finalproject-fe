b6ed2f80de4e3fdfc842ef67342057f07ce00afe
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-04-26 16:36:53 +0700

    profile and ui payment

--- src/pages/Me/Me.jsx
+++ src/pages/Me/Me.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import "./style.css"
 import axios from 'axios';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useQuery } from 'react-query'
 import { AuthContext } from "../../context/AuthContext";
 import Header from '../../components/Header/Header';
+import * as Toast from "../../common/Toast/Toast"
 
 function Me() {
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
+    const [errors, setErrors] = useState({})
+    const [oldPassword, setOldPassword] =useState()
+    const [newPassword, setNewPassword] =useState()
+    const [confirmPass, setConfirmPass] = useState()
+    const [credentials, setCredentials] = useState({
+        name: '',
+        username: '',
+        firstname: '',
+        lastname: '',
+        phone: '',
+    });
     // const [userData, setUserData] = useState();
 
     const { data, error, isError, isLoading } = useQuery(['user'], getUser)
@@ -25,7 +37,82 @@ function Me() {
 
     if (data == null) return <> </>
 
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+        setErrors({...errors, [e.target.id]: "" });
+    };
+    const handleUpdateProfile = ()=>{
+        const validationError = {};
+        const regexPhone = /^(?:\+?84|0)(?:\d{9,10})$/
+        const regexSpecial = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+        if (!credentials.username.trim()) {
+            validationError.username = "Username is required, please enter"
+        } else if (credentials.username.length < 10 || credentials.username.length > 30) {
+            validationError.username = "Username must be between 10 and 30 characters"
+        }
+        if (!credentials.firstname.trim()) {
+            validationError.firstname = "First name is required, please enter"
+        } else if (regexSpecial.test(credentials.firstname)) {
+            validationError.firstname = "First name cannot contain special characters"
+        }
 
+        if (!credentials.lastname.trim()) {
+            validationError.lastname = "Last name is required, please enter"
+        } else if (regexSpecial.test(credentials.lastname)) {
+            validationError.lastname = "Last name cannot contain special characters"
+        }
+
+        if (!credentials.phone.trim()) {
+            validationError.phone = "Phone Number is required, please enter"
+        } 
+        else if (!regexPhone.test(credentials.phone)) {
+            validationError.phone = "Phone Number is invalid, please enter"
+        }
+        setErrors(validationError);
+        if (Object.keys(validationError).length === 0) {
+            try{
+                const conf= window.confirm("Are you sure you want to update your profile?");
+                if(conf){
+                    axios.put(`https://finalproject-api.onrender.com/api/users/${user._id}`,{...credentials}).then((data)=> {
+                    Toast.toastSuccess("Update successfully") 
+                        setTimeout(()=>{
+                            window.location.reload();
+                        },3000)
+                    dispatch({ type: "LOGIN_SUCCESS", payload: data.data  })
+                }).catch((err)=> console.log(err))
+                }
+            }catch(err){
+                console.log("Error " + err.response.data);
+            }
+        }
+        
+    }
+
+    const handleChangePassword = () =>{
+        axios.post(`https://finalproject-api.onrender.com/api/auth/checkPassword`,{
+            email: user.email ,
+            oldPass: oldPassword,
+            newPass: newPassword,
+            confirmPass: confirmPass
+        }).then((data)=>{
+            Toast.toastSuccess(data.data.message) 
+            setTimeout(()=>{
+                window.location.reload();
+            },3000)
+            const email = {
+                email: user?.email,
+                subject: "Your Password has been changed",
+                html: `
+                <h1>Dear ${user?.lastname}</h1>
+                <p>Your Password has been changed at ${moment(new Date()).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                <p>Best regard.</p>
+                `
+            }
+            axios.post(`/emails/sendEmail`,email)
+        }).catch(err =>{
+            console.log(err?.response.data.message)
+        })
+    }
 
     return (
         <div className="profile">
@@ -45,67 +132,56 @@ function Me() {
                                             <h4 className="mb-4 mt-0">Contact detail</h4>
                                             <div className="col-md-6">
                                                 <label className="form-label">First Name *</label>
-                                                <input type="text" className="form-control" placeholder={data.firstname} aria-label="First name" />
+                                                <input id='firstname' type="text" className="form-control" placeholder={data?.firstname} aria-label="First name" onChange={handleChange}  />
+                                                {errors.firstname && <span className="text-danger">{errors.firstname}</span>}
                                             </div>
                                             <div className="col-md-6">
                                                 <label className="form-label">Last Name *</label>
-                                                <input type="text" className="form-control" placeholder={data.lastname} aria-label="Last name" />
+                                                <input id='lastname' type="text" className="form-control" placeholder={data?.lastname} aria-label="Last name" onChange={handleChange} />
+                                                {errors.lastname && <span className="text-danger">{errors.lastname}</span>}
                                             </div>
                                             <div className="col-md-6">
-                                                <label className="form-label">Birth Day</label>
-                                                <input type="text" className="form-control" value={data.birthday} aria-label="Birth Day" />
+                                                <label className="form-label">Email</label>
+                                                <input type="text" className="form-control" placeholder={data?.email} readOnly unselectable='true' aria-label="Birth Day"  />
                                             </div>
                                             <div className="col-md-6">
-                                                <label for="inputEmail4" className="form-label">Gender *</label>
-                                                <input type="text" className="form-control" id="gender" value={data.gender} />
+                                                <label className="form-label">UserName</label>
+                                                <input id='username' type="text" className="form-control" placeholder={data?.username} aria-label="Birth Day" onChange={handleChange} />
+                                                {errors.username && <span className="text-danger">{errors.username}</span>}
                                             </div>
                                             <div className="col-md-6">
                                                 <label className="form-label">Phone *</label>
-                                                <input type="number" className="form-control" placeholder="" aria-label="Phone Number" value={data.phone} />
-                                            </div>
-                                            <div className="col-md-6">
-                                                <label className="form-label">City</label>
-                                                <input type="text" className="form-control" placeholder={data.city} aria-label="ClassName" />
+                                                <input id='phone' type="number" className="form-control" placeholder={data?.phone} aria-label="Phone Number"  onChange={handleChange} />
+                                                {errors.phone && <span className="text-danger">{errors.phone}</span>}
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="btn btn-success p-3 mt-3 rounded-2 mb-5 w-100" onClick={handleUpdateProfile}>
+                                        Update Profile
+                                    </div>
+                                </div>
+                                <div className="col col-4">
 
-
-
-                                    <div className="bg-secondary-soft mt-3 px-4 py-5 rounded">
+                                <div className="bg-secondary-soft ms-2 px-4 py-5 rounded">
                                         <div className="row g-3">
                                             <h4 className="my-4">Change Password</h4>
 
                                             <div className="col-md-6">
                                                 <label htmlFor="exampleInputPassword1" className="form-label">Old password *</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword1" />
+                                                <input type="password" className="form-control" id="oldPassword" onChange={e=>setOldPassword(e.target.value)} />
                                             </div>
 
                                             <div className="col-md-6">
-                                                <label htmlFor="exampleInputPassword2" className="form-label">New password *</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword2" />
+                                                <label htmlFor="exampleInputPassword2" className="form-label" >New password *</label>
+                                                <input type="password" className="form-control" id="newPassword" onChange={e=> setNewPassword(e.target.value)}/>
                                             </div>
 
                                             <div className="col-md-12">
-                                                <label htmlFor="exampleInputPassword3" className="form-label">Confirm Password *</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword3" />
+                                                <label htmlFor="exampleInputPassword3" className="form-label">Confirm new password *</label>
+                                                <input type="password" className="form-control" id="confirmPass" onChange={e => setConfirmPass(e.target.value)} />
                                             </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div className="col-4">
-                                    <div className="bg-secondary-soft px-4 py-5 rounded">
-                                        <div className="row g-3">
-                                            <h4 className="mb-4 mt-0">Upload your profile photo</h4>
-                                            <div className="text-center">
-                                                <div className="square position-relative display-2 mb-3" >
-                                                    <img className="fas w-100 h-100 fa-fw fa-user position-absolute top-50 start-50 translate-middle text-secondary" src={data.avatar} alt=""/>
-                                                </div>
-                                                <input type="file" id="customFile" name="file" />
-                                                <label className="btn btn-success-soft btn-block" htmlFor="customFile">Upload</label>
-                                                <button type="button" className="btn btn-danger-soft">Remove</button>
-                                                <p className="text-muted mt-3 mb-0"><span className="me-1">Note:</span>Minimum size 300px x 300px</p>
+                                            <div className="btn btn-success" onClick={handleChangePassword}>
+                                                Change Password
                                             </div>
                                         </div>
                                     </div>
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -20,12 +20,12 @@ function Payment() {
                 <div className="row d-flex justify-content-center ">
                     <div className="col col-8 bg-light rounded-3 me-2 box-shadow">
 
-                    <p className='text-white fw-semibold text-center w-100 bg-color-primary rounded-top-3 pt-3 pb-3'>
+                        <p className='text-white fw-semibold text-center w-100 bg-color-primary rounded-top-3 pt-3 pb-3'>
                             We’re holding this price for you! Let’s complete your payment in 00:27:50
                         </p>
                         <div className="room_info p-2">
                             <div className="room_title d-flex justify-content-between align-items-center">
-                                <div className='fs-5 fw-semibold text-black'>{bookingData?.room?.name}</div>
+                                <div className='fs-4 fw-semibold text-black'>{bookingData?.room?.name}</div>
                                 <span>
                                     {[...Array(5)].map((_, index) => {
                                         return (
@@ -36,23 +36,23 @@ function Payment() {
                                     })}
                                 </span>
                             </div>
-                            <div className="fs-12 mt-1"><FontAwesomeIcon icon={faMapMarker} className='me-2' />{bookingData?.room?.address}</div>
+                            <div className="fs-12 mt-1"><FontAwesomeIcon icon={faMapMarker} className='me-2 fs-5' /> <span className='fs-6'>{bookingData?.room?.address}</span></div>
                             <div className="room_image mt-3">
                                 <img src={bookingData?.room?.imageUrls[Math.floor(Math.random() * 8)]} className='w-100 h-200 p-0 rounded-2' alt="" />
                             </div>
-                            <div className="time w-100 d-flex justify-content-between mt-4 align-items-center">
-                                <div className="check_in d-flex flex-wrap bg-white justify-content-center rounded-3 p-2 border">
-                                    <div className='text-black fs-12'>Check-in</div>
-                                    <div className='text-black fs-12 fw-bold'>{moment(bookingData?.checkin).format('MMMM Do YYYY')}</div>
-                                    <div className='fs-12'>From 7:00 AM</div>
+                            <div className="time w-100 d-flex justify-content-around mt-4 align-items-center">
+                                <div className="check_in d-flex flex-column align-items-center bg-white justify-content-center rounded-3 p-2 border">
+                                    <div className='text-black fs-3'>Check-in: </div>
+                                    <div className='text-black fs-3 fw-bold'>{moment(bookingData?.checkin).format('MMMM Do YYYY')}</div>
+                                    <div className='fs-4'>From 7:00 AM</div>
                                 </div>
                                 <div className="duration p-2">
-                                    <h3 className='border-bottom border-black fs-10 d-flex text-black'><span className='me-1 text-black fw-bold'>{bookingData?.totalday === 0 ? bookingData?.totalday + 1 : bookingData?.totalday}</span> day(s)</h3>
+                                    <h3 className='border-bottom border-black fs-3 d-flex text-black'><span className='me-1 text-black fw-bold fs-3'>{bookingData?.totalday === 0 ? bookingData?.totalday + 1 : bookingData?.totalday}</span> day(s)</h3>
                                 </div>
-                                <div className="check_in d-flex flex-wrap bg-white justify-content-center rounded-3 p-2 border">
-                                    <div className='text-black fs-12'>Check-out</div>
-                                    <div className='text-black fs-12 fw-bold'>{moment(bookingData?.checkout).format('MMMM Do YYYY')}</div>
-                                    <div className='fs-12'>Before 7:00 AM</div>
+                                <div className="check_in d-flex flex-column bg-white align-items-center justify-content-center rounded-3 p-2 border">
+                                    <div className='text-black fs-3'>Check-out</div>
+                                    <div className='text-black fs-3 fw-bold'>{moment(bookingData?.checkout).format('MMMM Do YYYY')}</div>
+                                    <div className='fs-4'>Before 7:00 PM</div>
                                 </div>
                             </div>
                         </div>
